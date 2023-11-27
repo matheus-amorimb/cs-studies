55a957619aee6f114eb081e423dd8fdf46cedea4
Matheus Batista <mbatista.sarti@gmail.com>
2023-11-27 14:15:14 -0300

    first program in c#

---
+++ LeetCode/Problem_01/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Solution 
+{
+    public string MergeAlternately(string word1, string word2) 
+    {
+        return word1 + word2;
+
+    }
+
+
+    static void Main()
+    {
+
+        string result = new Solution().MergeAlternately("party", "rock");
+
+        Console.WriteLine(result);
+    
+    }
+
+}
